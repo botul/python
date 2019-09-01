2b974fe8a18714e10b39d0486b3e8f95d56a99dc
botul <pawel.botulinski@gmail.com>
2019-09-01 23:16:13 +0200

    Problem 1&2 solved

--- iteration3.py
+++ iteration3.py
@@ -1,7 +1,10 @@
+count = 0
+phrase = "hello, world"
+
 for iteration in range(5):
-	count = 0
-	while True:
-		for letter in "hello, world":
-			count += 1
-		print("Iteration " + str(iteration) + "; count is: " + str(count))
-		break
+	index = 0
+	while index < len(phrase):
+		count += 1
+		index += 1
+	print("Iteration " + str(iteration) + "; count is: " + str(count))
+
